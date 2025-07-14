3915768303b0c3f3cbe3e46b89fc3a896388e313
Faisal-pixel <adamsfaisal2003@gmail.com>
2025-07-14 01:46:13 +0100

    updated the buy tickets top bar

--- src/components/home_page/components/buy-tickets.components.tsx
+++ src/components/home_page/components/buy-tickets.components.tsx
@@ -1,14 +1,25 @@
 import { CalendarTopBarLogo, XCloseBuyYourTicketIcon } from '@/assets/icons';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
-import React from 'react'
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 // type Props = {}
 
 const BuyTicketsComponent = () => {
+  const buyYourTicketUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdWdmmPM-PrSUNN6J_HqRAQRdwbmz_B7AnuQzaWXY8zKoMNRA/viewform"; // Replace with the actual URL
+  const [shouldShowTicketComponent, setShouldShowTicketComponent] = useState(true);
+
+  const handleClose = () => {
+    // Logic to close the ticket component
+    setShouldShowTicketComponent(false);
+    console.log("Close button clicked");
+  };
+  console.log("BuyTicketsComponent rendered");
+  console.log("shouldShowTicketComponent:", shouldShowTicketComponent);
   return (
     <>
-        <div className="bg-[rgba(105,65,198,1)] text-white py-3 px-5">
+        <div className={`bg-[rgba(105,65,198,1)] text-white py-3 px-5 ${shouldShowTicketComponent ? 'block' : 'hidden'}`}>
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row md:items-center md:justify-between ">
           <div className="flex items-center gap-3 mb-4 md:mb-0">
             <Image
@@ -18,24 +29,30 @@ const BuyTicketsComponent = () => {
             />
 
             <div className="flex flex-col">
-              <span className="text-base font-semibold">
+              {/* <span className="text-base font-semibold">
                 Explore the future of branding, storytelling, and digital impact
                 in an inspiring live experience.
+              </span> */}
+              <span className="text-base font-semibold">
+                The Eniivy Experience (Movies & Conversations)
               </span>
               <span className="text-[rgba(233,215,254,1)] leading-6">
-                October 12-14, 2024 • Lekki Phase 1, Lagos, Nigeria
+                July 27th, 2025
               </span>
             </div>
 
             <Image
               src={XCloseBuyYourTicketIcon}
               alt="Close Ticket Icon"
-              className="inline-block md:hidden cursor-pointer self-start"
+              className="inline-block ml-auto md:hidden cursor-pointer self-start"
+              onClick={handleClose}
             />
           </div>
-          <Button className="px-4 py-[10px] bg-white text-black cursor-pointer hover:bg-white">
-            Buy your Ticket
-          </Button>
+          <Link href={buyYourTicketUrl} className='w-full md:w-auto' target='_blank' rel='noopener noreferrer'>
+            <Button className="px-4 py-[10px] w-full bg-white text-black cursor-pointer hover:bg-white">
+              Buy your Ticket
+            </Button>
+          </Link>
         </div>
       </div>
     </>
