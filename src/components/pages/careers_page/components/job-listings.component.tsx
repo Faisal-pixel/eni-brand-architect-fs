8a32f1284ae32adba0a538aec0bb47f371164a2f
Faisal-pixel <adamsfaisal2003@gmail.com>
2025-07-24 00:02:27 +0100

    taking a break

--- src/components/pages/careers_page/components/job-listings.component.tsx
+++ src/components/pages/careers_page/components/job-listings.component.tsx
@@ -166,7 +166,7 @@ const JobListings = () => {
                       <span className="text-sm text-gray-600 font-medium">
                         {job.type}
                       </span>
-                      <button className="bg-green-700 hover:bg-green-800 text-white px-4 py-2 md:px-6 md:py-2 rounded-lg font-medium transition-colors duration-200 transform hover:scale-105 text-sm md:text-base">
+                      <button className="bg-green-700 cursor-pointer hover:bg-green-800 text-white px-4 py-2 md:px-6 md:py-2 rounded-lg font-medium transition-colors duration-200 transform hover:scale-105 text-sm md:text-base">
                         Apply
                       </button>
                     </div>
